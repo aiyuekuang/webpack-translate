9380e5d7c5fdf027027bc80235c1d889b46a014f
2092186738@qq.com <2092186738@qq.com>
2021-09-24 09:28:29 +0800

    版本迭代

--- src/translate/main.mjs
+++ src/translate/main.mjs
@@ -17,40 +17,61 @@ export default class TranslateMain {
         /** 输出的语言文件相对项目根目录的地址 */
         path: './src/components/locales/',
         /** 需作为源语言进行翻译的文件 */
-        source: '../src/components/locales/zh_CN.js'
+        source: '../src/components/locales/zh_CN.js',
+        /** 输出文件的其他名称设置，与上方translateTypes的数组一一对应即可，注意必须保持一致*/
+        outputFileName: [],
+        /** 翻译源文件的语言简码*/
+        from: "zh-CN"
     }
 
     constructor(config) {
-        this.translateTypes = config.translateTypes;
-        this.suffix = config.suffix
-        this.path = config.path
-        this.source = config.source
+        if (config.translateTypes) {
+            this.translateTypes = config.translateTypes;
+        }
+        if (config.suffix) {
+            this.suffix = config.suffix;
+        }
+        if (config.path) {
+            this.path = config.path;
+        }
+        if (config.source) {
+            this.source = config.source;
+        }
+        if (config.outputFileName) {
+            this.outputFileName = config.outputFileName;
+        }
+        if (config.from) {
+            this.from = config.from;
+        }
+
 
         this.file = fs.readFileSync(this.source, "utf-8");
         this.file = this.file.split("export default")[1];
         this.file = (new Function("return " + this.file))();
-        console.log(typeof this.file)
     }
 
     run() {
-        for (let i of this.translateTypes) {
-            let file = path.resolve(this.path + "/" + i + "." + this.suffix)
-            Symbol[i] = cloneop(this.file);
+        for (let i of this.translateTypes.keys()) {
+            let file = path.resolve(this.path + "/" + this.translateTypes[i] + "." + this.suffix)
+            if (this.outputFileName[i]) {
+                file = path.resolve(this.path + "/" + this.outputFileName[i] + "." + this.suffix)
+            }
+            Symbol[this.translateTypes[i]] = cloneop(this.file);
             let str = ""
             this.index = 0
-            this.objectLoop(Symbol[i], (data, t) => {
+            this.objectLoop(Symbol[this.translateTypes[i]], (data, t) => {
                 str += data[t] + this.symbol
             })
 
-            translate(str, {from: "zh-CN", to: i})
+            translate(str, {from: this.from, to: this.translateTypes[i]})
                 .then((data) => {
                     let splicing = (data.text).split(this.symbol)
                     this.index = 0
-                    this.objectLoop(Symbol[i], (data, t) => {
+                    this.objectLoop(Symbol[this.translateTypes[i]], (data, t) => {
                         // console.log(112233,splicing,this.index)
                         data[t] = splicing[this.index]
                     });
-                    fs.writeFile(file, "export default " + JSON.stringify(Symbol[i], null, 4), {encoding: 'utf8'}, err => {
+                    fs.writeFile(file, "export default " + JSON.stringify(Symbol[this.translateTypes[i]], null, 4), {encoding: 'utf8'}, err => {
                     })
                 })
                 .catch(console.error);
@@ -65,8 +86,11 @@ export default class TranslateMain {
     path = './src/components/locales/'
     source = '../src/components/locales/zh_CN.js'
     file = null
+    outputFileName = []
+    from = "zh-CN"
 
-    objectLoop(obj, fun = () => {}) {
+    objectLoop(obj, fun = () => {
+    }) {
         for (let i in obj) {
             if (typeof (obj[i]) === "object") {
                 this.objectLoop(obj[i], fun)
